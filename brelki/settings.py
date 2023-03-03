be106ff21a516482599104954827ee905d491fc3
alexeykurkin <fryonfuturama@gmail.com>
2023-03-03 15:26:07 +0300

    comment date correct format

--- brelki/settings.py
+++ brelki/settings.py
@@ -110,13 +110,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
+TIME_ZONE = 'Europe/Moscow'
 
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
+USE_I18N = False
 
 USE_TZ = True
+
 
 
 # Static files (CSS, JavaScript, Images)
