d2165d4d6ffe65498a3f3e254036ae18d5038853
Дарья Петухова <pda3004@bk.ru>
2023-11-06 22:04:38 +0300

    front_korzina

--- brelki/static/js/cart_json.js
+++ brelki/static/js/cart_json.js
@@ -1,4 +1,16 @@
 $(function() {
+
+    cartBlock = $('.cart-block');
+    cartClearButton = $('#cart-clear');
+
+    $('#toggle_cart_button').click(function() {
+        if (cartBlock.is(':hidden')) {
+            cartBlock.slideToggle();
+        } else if (cartBlock.is(':visible')) {
+            cartBlock.slideToggle();
+        }
+    });
+
     $('#add-cart-form').submit(function(event) {
         event.preventDefault();
         $.ajax({
@@ -10,27 +22,45 @@ $(function() {
             },
             dataType: 'json',
             success: function(response) {
+
+                $('.cart-item-count').text(parseInt(response.cart_item_count + 1));
+
+                $('#toggle_cart_button').html(`Корзина (${response.user_cart_len})`);
+                $('.no-cart-items-title').hide();
+
+                if (cartBlock.is(':hidden')) {
+                    cartBlock.slideToggle();
+                }
+                cartClearButton.show();
+
                 const urlParams = new URLSearchParams(window.location.search);
                 keychainId = urlParams.get('id');
                 itemFound = false;
                 $('.cart-item').each(function() {
-
                     if ($(this).children('h3').attr('id') == keychainId) {
                         itemFound = true;
                         keychainCount = parseInt($(this).children('h3').next().text());
                         $(this).children('h3').next().text(keychainCount + 1);
+                        if (cartBlock.is(':hidden')) {
+                            cartBlock.slideToggle();
+                        }
                     } 
                 });
 
                 if (!itemFound) {
                     $('#cart-list').append(`
-                    <div class="cart-item">
-                        <img src="${response.img}" width="25px" height="25px">
-                        <h3 id="${response.id}">${response.title}</h3>
-                        <h2>1</h2>
-                    </div>
+                    <a href="/keychain?id=${keychainId}">
+                        <div class="cart-item">
+                            <img src="${response.img}" width="25px" height="25px">
+                            <h3 id="${response.id}">${response.title}</h3>
+                            <h2>1</h2>
+                        </div>
+                    </a>
                     `);
                     $('#cart-submit-minus').show();
+                    $('#cart-submit-add').hide();
+                    $('#cart-submit-plus').show();
+                    $('.cart-item-count-block').show();
                 }
 
             },
@@ -42,6 +72,7 @@ $(function() {
     });
 
     $('#substract-cart-form').submit(function(event) {
+
         event.preventDefault();
         $.ajax({
             type: 'post',
@@ -52,22 +83,45 @@ $(function() {
             },
             dataType: 'json',
             success: function(response) {
+
+                if (response.user_cart_len == 0) {
+                    cartClearButton.hide();
+                    $('#cart-submit-minus').hide();
+                    $('#cart-submit-add').show();
+                    $('#cart-submit-plus').hide();
+                    $('.cart-item-count-block').hide();
+                    $('.no-cart-items-title').show();
+                }
+
+                $('#toggle_cart_button').html(`Корзина (${response.user_cart_len})`);
+
+                if (cartBlock.is(':hidden')) {
+                    if (cartBlock.is(':hidden')) {
+                        cartBlock.slideToggle();
+                    }
+                }
+                // cartClearButton.show();
+
                 const urlParams = new URLSearchParams(window.location.search);
                 keychainId = urlParams.get('id');
+
                 $('.cart-item').each(function() {
                     if ($(this).children('h3').attr('id') == keychainId) {
                         keychainCount = parseInt($(this).children('h3').next().text());
                         if ($(this).children('h3').next().text() == '1') {
-                            $(this).remove()
+                            $(this).remove();
                             $('#cart-submit-minus').hide();
+                            $('#cart-submit-plus').hide();
+                            $('#cart-submit-add').show();
+                            $('.cart-item-count-block').hide();
                         } else {
                             $(this).children('h3').next().text(keychainCount - 1);
+                            $('.cart-item-count').text(parseInt(response.cart_item_count - 1));
                         }
-                    } 
+                    }
                 });
             },
             error: function(response) {
-                console.log(13228);
                 console.log('error: ', response)
             }
         });
@@ -85,9 +139,18 @@ $(function() {
             },
             dataType: 'json',
             success: function(response) {
+                $('#toggle_cart_button').html('Корзина (0)');
+                $('.no-cart-items-title').show();
                 $('.cart-item').each(function() {
                     $(this).remove();
+                    $('#cart-submit-minus').hide();
+                    cartBlock.hide();
+                    cartClearButton.hide();
                 });
+                $('#cart-submit-minus').hide();
+                $('#cart-submit-add').show();
+                $('#cart-submit-plus').hide();
+                $('.cart-item-count-block').hide();
             },
             error: function(response) {
                 console.log('error: ', response)
