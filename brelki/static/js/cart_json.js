59503e8f0c7368afb05d73d03ac7db66f068d410
alexeykurkin <fryonfuturama@gmail.com>
2023-12-04 15:28:44 +0300

    cart js file optimized

--- brelki/static/js/cart_json.js
+++ brelki/static/js/cart_json.js
@@ -1,160 +1,141 @@
 $(function() {
 
-    cartBlock = $('.cart-block');
-    cartClearButton = $('#cart-clear');
+    const cartBlock = $('.cart-block'); // блок корзины
+    const cartList = $('#cart-list'); // div со списком брелков (cart-item) в блоке корзины
+    const cartClearButton = $('#cart-clear'); // кнопка очистки корзины
+    const keychainCountElt = $('#cartKeychainCount'); // кол-во брелков в корзине
+    const cartSumElt = $('#cartSumElement'); // сумма стоимостей всех брелков
+    const cartSumBlock = $('.cart-sum-title');
+    const noCartItemsTitle = $('#noCartItemsTitle'); // надпись, что корзина пуста
+    const toggleCartButton = $('#toggle_cart_button'); // кнопка показа/скрытия корзины
+    const cartSubmitMinus = $('#cart-submit-minus'); // Кнопка "-" у счётчика брелков
+    const cartSubmitPlus = $('#cart-submit-plus'); // Кнопка "+" у счётчика брелков
+    const cartSubmitAdd = $('#cart-submit-add'); // Кнопка добавления брелка в корзину
 
-    $('#toggle_cart_button').click(function() {
-        if (cartBlock.is(':hidden')) {
-            cartBlock.slideToggle();
-        } else if (cartBlock.is(':visible')) {
-            cartBlock.slideToggle();
-        }
-    });
+    function handleCartAction(cartAction, form) {
+        form.submit(function(event) {
+            event.preventDefault();
+            $.ajax(
+                {
+                    type: 'post',
+                    url: this.action,
+                    data: {
+                        action: cartAction,
+                        csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
+                    },
+                    dataType: 'json',
+                    success: function(response) {
 
-    $('#add-cart-form').submit(function(event) {
-        event.preventDefault();
-        $.ajax({
-            type: 'post',
-            url: this.action,
-            data: {
-                action: 'plus',
-                csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
-            },
-            dataType: 'json',
-            success: function(response) {
-                $('.cart-sum-title').show();
-                $('.cart-sum').text(parseInt(response.cart_sum))
+                        const cartKeychains = $('.cart-item');
+                        let cartSum;
+                        let cartLen
+                        let cartItemCount;
+                        let keychainId;
+                        let keychainImg;
+                        let keychainTitle;
 
-                $('.cart-item-count').text(parseInt(response.cart_item_count));
+                        if (cartAction != 'delete') {
+                            cartItemCount = response.cart_item_count;
+                            keychainId = response.id;
+                            keychainImg = response.img;
+                            keychainTitle = response.title;
+                            cartSum = response.cart_sum;
+                            cartLen = response.user_cart_len;
+                        } else {
+                            cartLen = 0;
+                            cartSum = 0;
+                            cartItemCount = 0;
+                        }
 
-                $('#toggle_cart_button').html(`Корзина (${response.user_cart_len})`);
-                $('.no-cart-items-title').hide();
+                        function handleAddPlusMinusDisplay(mode) {
+                            console.log(mode);
+                            if (mode == 'hide_add') {
+                                cartSubmitAdd.hide();
+                                cartSubmitPlus.show();
+                                cartSubmitMinus.show();
+                                keychainCountElt.show();
+                            } else if (mode == 'show_add') {
+                                cartSubmitAdd.show();
+                                cartSubmitPlus.hide();
+                                cartSubmitMinus.hide();
+                                keychainCountElt.hide();
+                            }
+                        }
 
-                if (cartBlock.is(':hidden')) {
-                    cartBlock.slideToggle();
-                }
-                cartClearButton.show();
+                        function handleEmptyCart(cartLen) {
+                            if (cartLen == 0) {
+                                cartClearButton.hide();
+                                cartSumBlock.hide();
+                                noCartItemsTitle.show();
+                                handleAddPlusMinusDisplay(mode='show_add');
+                                cartKeychains.each(function() {
+                                    $(this).remove();
+                                });
+                            } else {
+                                cartClearButton.show();
+                                cartSumBlock.show();
+                                noCartItemsTitle.hide();
+                                handleAddPlusMinusDisplay(mode='hide_add');
+                            }
+                        }
 
-                keychainId = response.id;
-                itemFound = false;
-                $('.cart-item').each(function() {
-                    if ($(this).children('h3').attr('id') == keychainId) {
-                        itemFound = true;
-                        $(this).children('h3').next().text(response.cart_item_count);
+                        function updateSumAndCount(cartItemCount, cartSum, cartLen) {
+                            keychainCountElt.text(cartItemCount);
+                            cartSumElt.text(cartSum);
+                            toggleCartButton.html(`Корзина (${cartLen})`);
+                        }
+
+                        function appendKeychainToCart(keychainData) {
+                            cartList.append(`
+                                <a href="/keychain/${keychainData.keychainId}">
+                                    <div class="cart-item">
+                                        <img src="/${keychainData.keychainImg}" width="25px" height="25px">
+                                        <h3 id="${keychainData.keychainId}">${response.title}</h3>
+                                        <h2>1</h2>
+                                    </div>
+                                </a>
+                            `);
+                        }
+
                         if (cartBlock.is(':hidden')) {
                             cartBlock.slideToggle();
                         }
-                    } 
-                });
 
-                if (!itemFound) {
-                    $('#cart-list').append(`
-                    <a href="/keychain/${keychainId}">
-                        <div class="cart-item">
-                            <img src="/${response.img}" width="25px" height="25px">
-                            <h3 id="${response.id}">${response.title}</h3>
-                            <h2>1</h2>
-                        </div>
-                    </a>
-                    `);
-                    $('#cart-submit-minus').show();
-                    $('#cart-submit-add').hide();
-                    $('#cart-submit-plus').show();
-                    $('.cart-item-count-block').show();
-                }
+                        handleEmptyCart(cartLen);
+                        
+                        let cartKeychainFound = false;
+                        cartKeychains.each(function() {
+                            let cartKeychainTitle = $(this).children('h3');
+                            let cartKeychainId = $(this).children('h3').attr('id');
+                            if (cartKeychainId == keychainId) {
+                                cartKeychainFound = true;
+                                if (cartItemCount == 0) {
+                                    $(this).remove();
+                                    handleAddPlusMinusDisplay(mode='show_add');
+                                } else {
+                                    cartKeychainTitle.next('h2').text(cartItemCount);
+                                }
+                            }
+                        });
+                        
+                        // Изменение суммы покупок и количества брелка в корзине
+                        updateSumAndCount(cartItemCount, cartSum, cartLen);
 
-            },
-            error: function(response) {
-                console.log('error: ', response)
-            }
-        });
-
-    });
-
-    $('#substract-cart-form').submit(function(event) {
-
-        event.preventDefault();
-        $.ajax({
-            type: 'post',
-            url: this.action,
-            data: {
-                action: 'minus',
-                csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
-            },
-            dataType: 'json',
-            success: function(response) {
-                $('.cart-sum').text(parseInt(response.cart_sum));
-                $('.cart-item-count').text(parseInt(response.cart_item_count));
-
-                if (response.user_cart_len == 0) {
-                    cartClearButton.hide();
-                    $('#cart-submit-minus').hide();
-                    $('#cart-submit-add').show();
-                    $('#cart-submit-plus').hide();
-                    $('.cart-item-count-block').hide();
-                    $('.no-cart-items-title').show();
-                    $('.cart-sum-title').hide();
-                }
-
-                $('#toggle_cart_button').html(`Корзина (${response.user_cart_len})`);
-
-                if (cartBlock.is(':hidden')) {
-                    if (cartBlock.is(':hidden')) {
-                        cartBlock.slideToggle();
-                    }
-                }
-                // cartClearButton.show();
-
-                keychainId = response.id
-
-                $('.cart-item').each(function() {
-                    if ($(this).children('h3').attr('id') == keychainId) {
-                        $(this).children('h3').next().text(response.cart_item_count);
-                        if (response.cart_item_count == 0) {
-                            $(this).remove();
-                        } else {
-                            $(this).children('h3').next().text(response.cart_item_count);
+                        if (!cartKeychainFound) {
+                            let keychainData = {'keychainId': keychainId, 'keychainTitle': keychainTitle, 'keychainImg': keychainImg};
+                            if (cartAction != 'delete') {
+                                appendKeychainToCart(keychainData);
+                            }
                         }
                     }
-                });
-            },
-            error: function(response) {
-                console.log('error: ', response)
-            }
+                }
+            );
         });
+    }
 
-    });
-
-    $('#delete-cart-form').submit(function(event) {
-        event.preventDefault();
-        $.ajax({
-            type: 'post',
-            url: this.action,
-            data: {
-                action: 'delete',
-                csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
-            },
-            dataType: 'json',
-            success: function(response) {
-                $('#toggle_cart_button').html('Корзина (0)');
-                $('.no-cart-items-title').show();
-                $('.cart-sum-title').hide();
-                $('.cart-item').each(function() {
-                    $(this).remove();
-                    $('#cart-submit-minus').hide();
-                    cartBlock.hide();
-                    cartClearButton.hide();
-                });
-                $('#cart-submit-minus').hide();
-                $('#cart-submit-add').show();
-                $('#cart-submit-plus').hide();
-                $('.cart-item-count-block').hide();
-            },
-            error: function(response) {
-                console.log('error: ', response)
-            }
-        });
-
-    });
+    toggleCartButton.on('click', () => cartBlock.slideToggle());
+    handleCartAction('plus', $('#add-cart-form'));
+    handleCartAction('minus', $('#substract-cart-form'));
+    handleCartAction('delete', $('#delete-cart-form'));
 
 });
